4304c50d0aecd9fd0db63e262a38de5118d3437f
Anish Patel <anishpatel39@gmail.com>
2020-02-01 12:55:08 -0500

    changes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var PORT = process.env.PORT || 8080;
 //APP DATA & ROUTING
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes.js')(app);
