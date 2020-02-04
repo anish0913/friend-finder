e21240dc5402e7fe2001958764669bdfe60bdc44
Anish Patel <anishpatel39@gmail.com>
2020-02-03 19:48:04 -0500

    updated read me

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Friend Finder
 
 # https://polar-mountain-79138.herokuapp.com
+
+In this activity, you'll build a compatibility-based "FriendFinder" application -- basically a dating app. This full-stack site will take in results from your users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.
+
+This survey has 10 questions Each answer is scaled from 1 to 5 based on how much the user agrees or disagrees with a question. After complete the survey you will be matached up with the most compatible friend
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,19 @@
 //Array of friends
-
 var buddy = [{
   "name": "Matt",
-  "photo": "example",
+  "photo": "https://mario.nintendo.com/assets/img/home/intro/mario-pose2.png",
   "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
 },{
   "name": "Neel",
-  "photo": "example",
+  "photo": "https://www.tvovermind.com/wp-content/uploads/2018/08/Naruto-750x415-750x415.jpg",
   "scores": [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
 },{
-  "name": "Pooja",
-  "photo": "example",
+  "name": "Ryan",
+  "photo": "https://i.pinimg.com/originals/56/4b/4e/564b4e2e3c2d2f8ef17798b97c0628b0.jpg",
   "scores": [5, 5, 1, 1, 3, 2, 4, 1, 3, 5]
 },{
   "name": "Jasmine",
-  "photo": "example",
+  "photo": "https://vignette.wikia.nocookie.net/disney/images/c/cd/Profile_-_Jasmine.jpeg/revision/latest?cb=20190312021628",
   "scores": [4, 4, 5, 6, 7, 3, 5, 4, 1, 1]
 }];
 
